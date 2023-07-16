2cb2f1121a75ebd8f010da9c06917cee2e3c20f8
Anubhav Lavania <anulav2@yahoo.com>
2023-07-16 18:45:42 -0500

    modified providers.tf

--- providers.tf
+++ providers.tf
@@ -8,17 +8,17 @@ terraform {
   }
 }
 
-#  required_providers {
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = "~> 5.0"
-#    }
-#  }
-#}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
 
 # Configure the AWS Provider
-#provider "aws" {
-#  region = "us-east-1"
-#}
-#}
+provider "aws" {
+  region = "us-east-1"
+}
+
 
